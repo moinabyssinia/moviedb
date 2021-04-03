5e29e87ec238f886e608a14514345798e9624c00
moinabyssinia <michaelg.tadesse@gmail.com>
2021-04-03 11:35:16 -0400

    project complete

--- src/components/searchMovies.js
+++ src/components/searchMovies.js
@@ -1,20 +1,25 @@
-import React from 'react'
-
+import React, { useState } from 'react';
+import MovieCard from './movieCard';
 const SearchMovies = () => {
+
+    // states - input query, movies 
+    const [query, setQuery] = useState('');
+
+    // create the state for movies, and update that state appropriate
+    const [movies, setMovies] = useState([]);
+
 
     const searchMovies = async (e) => {
         // prevent the form from submitting
         e.preventDefault();
-        console.log("submitting");
-
-        const query = "October Baby";
-
+ 
         const url = `https://api.themoviedb.org/3/search/movie?api_key=42c140a2fe0c3971953120a8a9c8f0d3&language=en-US&page=1&include_adult=false&query=${query}`;
 
         try {
             const res = await fetch(url);
-            const data = await res.json();
-            console.log(data);
+            const data = await res.json(); // returns a promise
+            console.log(data.results);
+            setMovies(data.results);
         }
         catch(err) {
             console.log(err);
@@ -23,13 +28,26 @@ const SearchMovies = () => {
     }
 
     return (
-        <form className = "form" onSubmit = { searchMovies }>
-            <label className = "label" htmlFor = "query">Movie Name</label>
+        <>
 
-            <input className = "input" type = "text" name = "query" placeholder = "i.e. Jurassic Park"></input>
+            <form className = "form" onSubmit = { searchMovies }>
+                <label className = "label" htmlFor = "query">Movie Name</label>
 
-            <button className = "button" type = "submit">Search</button>
-        </form>
+                <input className = "input" type = "text" name = "query" placeholder = "i.e. Jurassic Park"
+                value = { query } onChange = {(e) => setQuery(e.target.value)}
+                />
+
+                <button className = "button" type = "submit">Search</button>
+            </form>
+
+            <div className = "card-list">
+                {movies.filter(movie => movie.poster_path).map(movie => (
+                   <MovieCard movie = { movie } key = {movie.id}/>
+                ))}
+            </div>
+
+        </>
+
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import SearchMovies from "./components/searchMovies";
 
+
 class Main extends React.Component {
   render() {
     return (
        <div className = "container">
-            <h1 className = "title">React Movie Search</h1>
+            <h1 className = "title">iMovie Search</h1>
             <SearchMovies/>
        </div>
     );
